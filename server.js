50ec1c6b8c91c71f400203fbdf7717774dff048f
志田洋斗 <shida@shidaMacBook-Air.local>
2021-05-24 18:11:48 +0900

    add public-dir

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app  = require("express")();
+const express = require("express");
+const app  = express();
 const http = require("http").createServer(app);
 const io   = require("socket.io")(http);
 const PORT = process.env.PORT || 5000;
@@ -9,14 +10,9 @@ let room_list = {};
 //                  };
 
 /**
- * アクセスがあったら各ファイルを返却
+ * 公開フォルダの指定
  */
-app.get("/", (req, res)=>{
-  res.sendFile(__dirname + "/index.html");
-});
-app.get("/style.css", (req, res)=>{
-  res.sendFile(__dirname + "/style.css");
-});
+app.use(express.static(__dirname + '/public'));
 
 /**
  * [イベント] ユーザーが接続
@@ -83,11 +79,8 @@ io.on("connection", (socket)=>{
 });
 
 /**
- * 3000番でサーバを起動する
+ * サーバを起動する
  */
-// http.listen(3000, ()=>{
-//   console.log("listening on *:3000");
-// });
 http.listen(PORT, () =>{
   console.log(`Listening on ${ PORT }`)
 });
