3b75c89ef7b7bd22e0631675b6fc22c78c8027c3
Hiroto-Shida <shida@shidaMacBook-Air.local>
2021-05-28 23:32:51 +0900

    design size update

--- public/main.js
+++ public/main.js
@@ -3,14 +3,50 @@
 //-------------------------------------
 const socket = io();
 
-// 参加者名前表示の関数
+// 参加者名表示の関数
 function parNamePrint(list) {
-const par_name = document.querySelector("#par-name");
-par_name_str = '';
-for (let i = 0; i < list.length; i++) {
-    par_name_str += list[i] + ', ';
+    const par_name = document.querySelector("#par-name");
+    par_name_str = '';
+    for (let i = 0; i < list.length; i++) {
+        par_name_str += list[i] + ', ';
+    }
+    par_name.innerHTML = `${par_name_str}`;
 }
-par_name.innerHTML = `${par_name_str}`;
+
+// 送信された(した)チャットを表示する関数
+function chatPrint(direction, name, text) {
+    const list = document.querySelector("#msglist");
+    const tr1 = document.createElement("tr");
+    const tr2 = document.createElement("tr");
+
+    const td_icon = document.createElement("td");
+    td_icon.setAttribute("rowspan", "2");
+    td_icon.classList.add('chat-icon-box');
+    const td_name = document.createElement("td");
+    const td_text = document.createElement("td");
+    const p_text = document.createElement("p");
+    p_text.classList.add('chat-text');
+
+    if (direction == "to_other") {
+        td_name.classList.add('text-right');
+        td_text.classList.add('text-right');
+    }else if(direction == "to_me"){
+        td_icon.setAttribute("valign", "top");
+        const div_icon = document.createElement("div");
+        div_icon.classList.add('chat-icon');
+        td_icon.appendChild(div_icon);
+        td_name.classList.add('text-left');
+        td_text.classList.add('text-left');
+    }
+
+    td_name.innerHTML = `${name}`;
+    p_text.innerHTML = `${text}`;
+    td_text.appendChild(p_text);
+    tr1.appendChild(td_icon);
+    tr1.appendChild(td_name);
+    tr2.appendChild(td_text);
+    list.appendChild(tr1);
+    list.appendChild(tr2);
 }
 
 /**
@@ -37,153 +73,88 @@ socket.on('update usernum', (list)=>{
  * [イベント] room名・名前が入力され，「参加」が押された
  */
 document.querySelector("#name-post").addEventListener("submit", (e)=>{
-// 規定の送信処理をキャンセル(画面遷移しないなど)
-e.preventDefault();
+    // 規定の送信処理をキャンセル(画面遷移しないなど)
+    e.preventDefault();
 
-// 入力されたroom名・名前を取得する
-const name = document.querySelector("#name");
-const room_name = document.querySelector("#room_name");
-if( (name.value === "") || (room_name.value === "") ){
-    return(false);
-}
+    // 入力されたroom名・名前を取得する
+    const name = document.querySelector("#name");
+    const room_name = document.querySelector("#room_name");
+    if( (name.value === "") || (room_name.value === "") ){
+        return(false);
+    }
 
-// Socket.ioサーバへ送信
-socket.emit("name post", {room_name: room_name.value, name: name.value});
+    // Socket.ioサーバへ送信
+    socket.emit("name post", {room_name: room_name.value, name: name.value});
 
-// 名前入力を非表示，チャットを表示
-document.querySelector("#name-post").style.display = "none"
-document.querySelector("#chat-box").style.display = "block"
+    // 名前入力を非表示．チャットを表示
+    document.querySelector("#name-post").style.display = "none"
+    document.querySelector("#chat-box").style.display = "flex"
 
-// 自分のルーム名・名前を表示
-document.querySelector("#room-name").innerHTML = `${room_name.value}`;
-document.querySelector("#user-name").innerHTML = `${name.value}`;
+    // 自分のルーム名・名前を表示
+    document.querySelector("#room-name").innerHTML = `${room_name.value}`;
+    document.querySelector("#user-name").innerHTML = `${name.value}`;
 
-// 名前ボックスを空にする
-room_name.value = "";
-name.value = "";
+    // 名前ボックスを空にする
+    room_name.value = "";
+    name.value = "";
 
-document.querySelector("#msg").focus();
+    document.querySelector("#msg").focus();
 });
 
 /**
  * [イベント] 名前が決定された
  */
 socket.on("membername-post", (list)=>{
-parNamePrint(list);
+    parNamePrint(list);
 });
 
 /**
- * [イベント] チャットを送信した
+ * [イベント] 自分がチャットを送信した
  */
 document.querySelector("#frm-post").addEventListener("submit", (e)=>{
-// 規定の送信処理をキャンセル(画面遷移しないなど)
-e.preventDefault();
+    // 規定の送信処理をキャンセル(画面遷移しないなど)
+    e.preventDefault();
 
-// ルーム名・送信者名前・入力内容 を取得する
-const room_name = document.querySelector("#room-name");
-const name = document.querySelector("#user-name");
-const msg = document.querySelector("#msg");
-if( msg.value === "" ){
-    return(false);
-}
+    // ルーム名・送信者名前・入力内容 を取得する
+    const room_name = document.querySelector("#room-name");
+    const name = document.querySelector("#user-name");
+    const msg = document.querySelector("#msg");
+    if( msg.value === "" ){
+        return(false);
+    }
 
-const list = document.querySelector("#msglist");
-const tr1 = document.createElement("tr");
-const tr2 = document.createElement("tr");
+    // Socket.ioサーバへ送信
+    socket.emit("post", {room_name: room_name.textContent, name: name.textContent, text: msg.value});
 
-const td = document.createElement("td");
-td.setAttribute("rowspan", "2");
-td.classList.add('chat-icon-box');
+    // 自分が送信したメッセージを表示
+    chatPrint("to_other", name.textContent, msg.value);
 
-const td_name = document.createElement("td");
-// td_name.classList.add('chat-name');
-td_name.classList.add('text-right');
+    // チャットボックスを空にする
+    msg.value = "";
 
-const td_icon = document.createElement("td");
-td_icon.setAttribute("rowspan", "2");
-td_icon.setAttribute("valign", "top");
-td_icon.classList.add('chat-icon-box');
-const div_icon = document.createElement("div");
-div_icon.classList.add('chat-icon');
-td_icon.appendChild(div_icon);
-
-const td_text = document.createElement("td");
-td_text.classList.add('text-right');
-const p_text = document.createElement("p");
-p_text.classList.add('chat-text');
-
-td_name.innerHTML = `${name.textContent}`;
-p_text.innerHTML = `${msg.value}`;
-td_text.appendChild(p_text);
-tr1.appendChild(td);
-tr1.appendChild(td_name);
-tr1.appendChild(td_icon);
-tr2.appendChild(td_text);
-list.appendChild(tr1);
-list.appendChild(tr2);
-
-// Socket.ioサーバへ送信
-socket.emit("post", {room_name: room_name.textContent, name: name.textContent, text: msg.value});
-
-// 一番下までスクロール
-const container = document.querySelector(".chat-main-box");
-container.scrollTop = container.scrollHeight;
-
-// チャットボックスを空にする
-msg.value = "";
+    // 最新を表示するため一番下までスクロール
+    const container = document.querySelector(".chat-main-box");
+    container.scrollTop = container.scrollHeight;
 });
 
 /**
- * [イベント] 誰かが発言した
+ * [イベント] 誰かがチャットを送信した
  */
 socket.on("member-post", (msg)=>{
-const list = document.querySelector("#msglist");
-const tr1 = document.createElement("tr");
-const tr2 = document.createElement("tr");
+    // 誰かが送信したメッセージを表示
+    chatPrint("to_me", msg.name, msg.text);
 
-const td_icon = document.createElement("td");
-td_icon.setAttribute("rowspan", "2");
-td_icon.setAttribute("valign", "top");
-td_icon.classList.add('chat-icon-box');
-const div_icon = document.createElement("div");
-div_icon.classList.add('chat-icon');
-td_icon.appendChild(div_icon);
-
-const td_name = document.createElement("td");
-// td_name.classList.add('chat-name');
-td_name.classList.add('text-left');
-
-const td = document.createElement("td");
-td.setAttribute("rowspan", "2");
-td.classList.add('chat-icon-box');
-
-const td_text = document.createElement("td");
-td_text.classList.add('text-left');
-const p_text = document.createElement("p");
-p_text.classList.add('chat-text');
-
-td_name.innerHTML = `${msg.name}`;
-p_text.innerHTML = `${msg.text}`;
-td_text.appendChild(p_text);
-tr1.appendChild(td_icon);
-tr1.appendChild(td_name);
-tr1.appendChild(td);
-tr2.appendChild(td_text);
-list.appendChild(tr1);
-list.appendChild(tr2);
-
-// 一番下までスクロール
-const container = document.querySelector(".chat-main-box");
-container.scrollTop = container.scrollHeight;
-
+    // 最新を表示するため一番下までスクロール
+    const container = document.querySelector(".chat-main-box");
+    container.scrollTop = container.scrollHeight;
 });
 
 /**
  * [イベント] ページの読込み完了
  */
 window.onload = ()=>{
-// テキストボックスを選択する
-document.querySelector("#room_name").focus();
-let style = "<link rel='stylesheet' href='style.css'>";
-$('head:last').after(style);
+    // テキストボックスを選択する
+    document.querySelector("#room_name").focus();
+    let style = "<link rel='stylesheet' href='style.css'>";
+    $('head:last').after(style);
 }
--- server.js
+++ server.js
@@ -29,20 +29,21 @@ io.on("connection", (socket)=>{
     room_name = msg.room_name; // room名前
     socket.join(room_name); // 入力されたルーム名のルームに参加
 
-    let tmp_room_list = [msg.name, msg.room_name];
-    room_list[socket.id] = tmp_room_list;
+    // socket.idに対応する 名前・ルーム を登録
+    room_list[socket.id] = [msg.name, msg.room_name];
 
+    // 同じルーム内の名前リストを作成・更新
     same_room_id_list = Array.from(socket.adapter.rooms.get(room_name))
     same_room_name_list = [];
     for (let i = 0; i < same_room_id_list.length; i++) {
-      same_room_name = room_list[same_room_id_list[i]][0];
-      same_room_name_list.push(same_room_name);
+      same_room_name_list.push( room_list[same_room_id_list[i]][0] );
     }
+    io.to(msg.room_name).emit("membername-post", same_room_name_list);
+
     console.log("---ユーザーがルームに入りました---");
     console.log("同ルーム内のidリスト：",same_room_id_list);
     console.log("ユーザーリスト",room_list);
     console.log("ルームメンバー名リスト",same_room_name_list);
-    io.to(msg.room_name).emit("membername-post", same_room_name_list);
   });
 
   // 文字が送信された時
@@ -63,13 +64,13 @@ io.on("connection", (socket)=>{
     console.log('全コネクション数', socket.client.conn.server.clientsCount);
 
     // ルーム内にだれもいない時以外
-    let a = socket.adapter.rooms.get(room_name);
-    if (a != null) {
-      same_room_id_list = Array.from(a);
+    let same_room_info = socket.adapter.rooms.get(room_name);
+    if (same_room_info != null) {
+      // 同じルーム内の名前リストを作成・更新
+      same_room_id_list = Array.from(same_room_info);
       same_room_name_list = [];
       for (let i = 0; i < same_room_id_list.length; i++) {
-        same_room_name = room_list[same_room_id_list[i]][0];
-        same_room_name_list.push(same_room_name);
+        same_room_name_list.push( room_list[same_room_id_list[i]][0] );
       }
       io.to(room_name).emit('update usernum', same_room_name_list);
     }
