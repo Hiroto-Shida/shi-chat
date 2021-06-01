0c2b346ce80abc881b413af6ff5ecd8a5912e1e8
Hiroto-Shida <shida@shidaMacBook-Air.local>
2021-06-01 17:29:19 +0900

    design update

--- public/main.js
+++ public/main.js
@@ -3,18 +3,27 @@
 //-------------------------------------
 const socket = io();
 
+let clientName;
+
 // 参加者名表示の関数
 function parNamePrint(list) {
     const par_name = document.querySelector("#par-name");
-    par_name_str = '';
+    par_name.innerHTML = '';
+    let first = true;
     for (let i = 0; i < list.length; i++) {
-        par_name_str += list[i] + ', ';
+        const span = document.createElement("span");
+        span.classList.add('par-name');
+        if (list[i] == clientName && first) {
+            span.classList.add('par-myname');
+            first = false;
+        }
+        span.innerHTML = list[i];
+        par_name.appendChild(span);
     }
-    par_name.innerHTML = `${par_name_str}`;
 }
 
 // 送信された(した)チャットを表示する関数
-function chatPrint(direction, name, text) {
+function chatPrint(direction, user_name, text, time) {
     const list = document.querySelector("#msglist");
     const tr1 = document.createElement("tr");
     const tr2 = document.createElement("tr");
@@ -27,9 +36,17 @@ function chatPrint(direction, name, text) {
     const p_text = document.createElement("p");
     p_text.classList.add('chat-text');
 
+    // 送信時刻
+    const span_time = document.createElement("span");
+    span_time.classList.add('emit-time');
+    span_time.innerHTML = ` ${time} `;
+
     if (direction == "to_other") {
         td_name.classList.add('text-right');
         td_text.classList.add('text-right');
+
+        td_name.innerHTML = `${user_name}`;
+        td_name.insertBefore(span_time, td_name.firstChild);
     }else if(direction == "to_me"){
         td_icon.setAttribute("valign", "top");
         const div_icon = document.createElement("div");
@@ -37,9 +54,11 @@ function chatPrint(direction, name, text) {
         td_icon.appendChild(div_icon);
         td_name.classList.add('text-left');
         td_text.classList.add('text-left');
+
+        td_name.innerHTML = `${user_name}`;
+        td_name.appendChild(span_time);
     }
 
-    td_name.innerHTML = `${name}`;
     p_text.innerHTML = `${text}`;
     td_text.appendChild(p_text);
     tr1.appendChild(td_icon);
@@ -77,14 +96,15 @@ document.querySelector("#name-post").addEventListener("submit", (e)=>{
     e.preventDefault();
 
     // 入力されたroom名・名前を取得する
-    const name = document.querySelector("#name");
+    const user_name = document.querySelector("#name");
     const room_name = document.querySelector("#room_name");
-    if( (name.value === "") || (room_name.value === "") ){
+    if( (user_name.value === "") || (room_name.value === "") ){
         return(false);
     }
+    clientName = user_name.value; // 名前をグローバル変数に保存
 
     // Socket.ioサーバへ送信
-    socket.emit("name post", {room_name: room_name.value, name: name.value});
+    socket.emit("name post", {room_name: room_name.value, name: user_name.value});
 
     // 名前入力を非表示．チャットを表示
     document.querySelector("#name-post").style.display = "none"
@@ -92,11 +112,11 @@ document.querySelector("#name-post").addEventListener("submit", (e)=>{
 
     // 自分のルーム名・名前を表示
     document.querySelector("#room-name").innerHTML = `${room_name.value}`;
-    document.querySelector("#user-name").innerHTML = `${name.value}`;
+    // document.querySelector("#user-name").innerHTML = `${user_name.value}`;
 
     // 名前ボックスを空にする
     room_name.value = "";
-    name.value = "";
+    user_name.value = "";
 
     document.querySelector("#msg").focus();
 });
@@ -117,20 +137,29 @@ document.querySelector("#frm-post").addEventListener("submit", (e)=>{
 
     // ルーム名・送信者名前・入力内容 を取得する
     const room_name = document.querySelector("#room-name");
-    const name = document.querySelector("#user-name");
+    // const user_name = document.querySelector("#user-name");
     const msg = document.querySelector("#msg");
     if( msg.value === "" ){
         return(false);
     }
 
+    // 送信時間を取得
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let nowtime = hour+':'+minute;
+
     // Socket.ioサーバへ送信
-    socket.emit("post", {room_name: room_name.textContent, name: name.textContent, text: msg.value});
+    socket.emit("post", {room_name: room_name.textContent, name: clientName, text: msg.value, time: nowtime});
 
     // 自分が送信したメッセージを表示
-    chatPrint("to_other", name.textContent, msg.value);
+    chatPrint("to_other", clientName, msg.value, nowtime);
 
     // チャットボックスを空にする
     msg.value = "";
+
+    // 送信ボタンを薄くする
+    document.getElementById('submit').classList.remove('submit-button');
 
     // 最新を表示するため一番下までスクロール
     const container = document.querySelector(".chat-main-box");
@@ -142,7 +171,7 @@ document.querySelector("#frm-post").addEventListener("submit", (e)=>{
  */
 socket.on("member-post", (msg)=>{
     // 誰かが送信したメッセージを表示
-    chatPrint("to_me", msg.name, msg.text);
+    chatPrint("to_me", msg.name, msg.text, msg.time);
 
     // 最新を表示するため一番下までスクロール
     const container = document.querySelector(".chat-main-box");
@@ -154,13 +183,11 @@ socket.on("member-post", (msg)=>{
  */
 let scrollControll = function(event) {
   let scrollarea = $(event.target).closest('.chat-main-box');
-//   let scrollArea2 = $(event.target).closest('.tablevalue > table');
   if (scrollarea.length > 0 && scrollarea.scrollTop() != 0 && scrollarea.scrollTop + scrollarea.clientHeight !== scrollarea.scrollHeight) {
-//   if (event.target === scrollarea && scrollarea.scrollTop !== 0 && scrollarea.scrollTop + scrollarea.clientHeight !== scrollarea.scrollHeight) {
     event.stopPropagation();
-    console.log("OK!!!!!")
+    // console.log("OK!!!!!")
   }else{
-    console.log("だめ")
+    // console.log("だめ")
     event.preventDefault();
   }
 };
@@ -178,6 +205,27 @@ scrollarea.addEventListener('scroll', function() {
   }
 });
 
+
+// チャット入力ボックスに１文字以上入力されたら送信可能
+window.addEventListener('DOMContentLoaded',function(){
+    document.getElementById('submit').disabled = true;
+    document.getElementById('msg').addEventListener('keyup',function(){
+        if (this.value.length < 1) {
+            document.getElementById('submit').disabled = true;
+            document.getElementById('submit').classList.remove('submit-button');
+        } else {
+            document.getElementById('submit').disabled = false;
+            document.getElementById('submit').classList.add('submit-button');
+        }
+    },false);
+    document.getElementById('msg').addEventListener('change',function(){
+        if (this.value.length < 1) {
+            document.getElementById('submit').disabled = true;
+            document.getElementById('submit').classList.remove('submit-button');
+        }
+    },false);
+},false);
+    
 
 
 /**
